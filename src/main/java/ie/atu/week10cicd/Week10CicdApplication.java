573e9b76c7c1a3441465ec70012bb712299beb5e
G00415457@atu.ie <G00415457@atu.ie>
2024-11-27 14:17:34 +0000

    Added to git

---
+++ src/main/java/ie/atu/week10cicd/Week10CicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week10cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week10CicdApplication.class, args);
+    }
+
+}
